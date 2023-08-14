f1082d602ee728b9aff086ab79e6716b472f5160
DaDoRiNZaNyA <123072513+DaDoRiNZaNyA@users.noreply.github.com>
2023-08-14 17:18:23 +0400

    files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const authRouter = require("./routes/auth.routes");
+const fileRouter = require("./routes/file.routes");
 const corsMiddleware = require("./middleware/cors.middleware");
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = config.get("serverPort");
 app.use(corsMiddleware);
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api/files", fileRouter);
 
 const start = async () => {
   try {
@@ -18,7 +20,9 @@ const start = async () => {
     app.listen(PORT, () => {
       console.log("server started on port", PORT);
     });
-  } catch (e) {}
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
