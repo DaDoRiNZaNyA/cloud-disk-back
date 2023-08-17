b5582a30c8d040d78dd7aeb5cd80c7cf91cc1972
DaDoRiNZaNyA <123072513+DaDoRiNZaNyA@users.noreply.github.com>
2023-08-17 18:28:08 +0400

    upload/download

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -6,6 +6,8 @@ const fileController = require("../controllers/fileController");
 const router = new Router();
 
 router.post("", authMiddleware, fileController.createDir);
+router.post("/upload", authMiddleware, fileController.uploadFile);
 router.get("", authMiddleware, fileController.getFiles);
+router.get("/download", authMiddleware, fileController.downloadFile);
 
 module.exports = router;
